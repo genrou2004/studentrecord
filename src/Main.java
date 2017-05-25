091afdb74c867766131f64daaa5fd56948e976c0
student <student@GBTC440J10LX.montgomerycollege.edu>
2017-05-25 15:01:26 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student std = new Student();
+		
+		std.setFirstName("Aster");
+		std.setLastName("Kebede");
+		std.setSex("Female");
+		std.setAge(60);
+		
+		
+		System.out.println(std.toString());
+
+	}
+
+}
---
+++ src/Student.java
@@ -0,0 +1,38 @@
+
+
+public class Student {
+
+	private String firstName;
+	private String LastName;
+	private String sex;
+	private int age;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", LastName=" + LastName + ", sex=" + sex + ", age=" + age + "]";
+	}
+	
+}
